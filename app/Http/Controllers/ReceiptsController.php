dbd56e1e54965a236e462d1cae64c8eb677c6e55
FlorianKe <fl-keller@outlook.de>
2026-01-18 18:33:07 +0100

    refactor: catch errors

--- app/Http/Controllers/ReceiptsController.php
+++ app/Http/Controllers/ReceiptsController.php
@@ -10,6 +10,7 @@ use App\Services\OcrService;
 use Illuminate\Container\Attributes\CurrentUser;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -32,14 +33,20 @@ class ReceiptsController extends Controller
      */
     public function store(#[CurrentUser] User $user, StoreReceiptsRequest $request, AiParserServices $aiParserServices): RedirectResponse
     {
-        $ocrText = OcrService::extractText($request->file('receipt')->getPathname());
+        try {
+            $ocrText = OcrService::extractText($request->file('receipt')->getPathname());
 
-        $receipt = $aiParserServices->parseReceipt($ocrText);
+            $receipt = $aiParserServices->parseReceipt($ocrText);
 
-        Receipts::query()->create([
-            'user_id' => $user->id,
-            ...$receipt,
-        ]);
+            Receipts::query()->create([
+                'user_id' => $user->id,
+                ...$receipt,
+            ]);
+        } catch (\Throwable) {
+            throw ValidationException::withMessages([
+                'receipt' => 'OCR failed. Please try later again.',
+            ]);
+        }
 
         // TODO: store receipt image in storage
 
