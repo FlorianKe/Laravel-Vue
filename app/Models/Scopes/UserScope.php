4327e394265f81d7e4d70ab669f937fc8e129a97
FlorianKe <fl-keller@outlook.de>
2026-01-19 16:03:14 +0100

    refactor: user helper function

--- app/Models/Scopes/UserScope.php
+++ app/Models/Scopes/UserScope.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Scopes;
 
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
@@ -10,9 +11,11 @@ class UserScope implements Scope
 {
     /**
      * Apply the scope to a given Eloquent query builder.
+     *
+     * @throws Exception
      */
     public function apply(Builder $builder, Model $model): void
     {
-        $builder->where('user_id', auth()->id());
+        $builder->where('user_id', user()->id);
     }
 }
